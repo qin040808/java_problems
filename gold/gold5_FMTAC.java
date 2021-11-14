1c10394b6a78fc25eb74a1252a527fcba120fca7
qin040808 <qin0808@naver.com>
2021-11-14 22:35:59 +0900

    plat5_sort
    sort the numbers to not touch the
    ascend/descending numbers
    with it being most ordering in ascending way possible

--- gold/gold5_FMTAC.java
+++ gold/gold5_FMTAC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class gold5_FMTAC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
---
+++ platinum/plat5_sort.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class plat5_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        boolean check = true;
+        int temp = 0;
+        boolean sec = true;
+        while (check) {
+            check = false;
+            for (int i = 0; i < arr.length-1; i++) {
+                if ((arr[i] + 1) == arr[i + 1]) {
+                    sec = true;
+                    check = true;
+                    for (int j = i; j < arr.length; j++) {
+                        if ((arr[i] + 1) < arr[j]) {
+                            temp = arr[i + 1];
+                            arr[i + 1] = arr[j];
+                            arr[j] = temp;
+                            sec = false;
+                            break;
+                        }
+                    }
+                    if (sec) {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+    }
+}
