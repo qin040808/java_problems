fcc957780a50423745215c0c98e0f51d9f901aba
qin0808 <qin0808@naver.com>
2021-11-15 16:57:02 +0900

    refactored file arrange
    sort by problem number from boj

--- boj/boj1011.java
+++ boj/boj1011.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class gold5_FMTAC {
+public class boj1011 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
