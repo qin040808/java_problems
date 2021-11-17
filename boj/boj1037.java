988b48c97cf5bacfbbd854d5a55f20579cc3c1cb
qin0808 <qin0808@naver.com>
2021-11-17 23:38:07 +0900

    boj1037
    약수들을 받아
    무슨수인지 맞추는 프로그램

---
+++ boj/boj1037.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class boj1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum[] = new int[n];
+        for (int i = 0; i < sum.length; i++) {
+            sum[i]=sc.nextInt();
+        }
+        int min=sum[0];
+        int max=0;
+        for (int i : sum) {
+            if(i>max){
+                max=i;
+            }
+            if(i<min){
+                min=i;
+            }
+        }
+        System.out.println(max*min);
+    }
+}
