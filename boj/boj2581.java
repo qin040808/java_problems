12bfe03b117c702e944a182181b127bb2b7a7404
qin040808 <qin0808@naver.com>
2021-11-20 23:47:29 +0900

    boj 2581
    get all primary numbers between to numbers,
    get the sum of them and the min

---
+++ boj/boj2581.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class boj2581 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int c= in.nextInt();
+        int sum = 0;
+        int min=c;
+        for (int i = a; i <=c ; i++) {
+            if(ifreal(i)){
+                if(min>i){
+                    min=i;
+                }
+                sum+=i;
+            }
+        }
+        if(sum==0){
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(sum);
+        System.out.println(min);
+    }
+    public static boolean ifreal(int i){
+        if(i==1){
+            return false;
+        }
+        for (int j = 2; j < i; j++) {
+            if(i%j==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
