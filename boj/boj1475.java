c4aa79b48b6322a8b373e4944485049435c4da45
qin040808 <qin0808@naver.com>
2021-11-22 23:45:40 +0900

    boj1475
    recive a number
    get how many 0-9 number packes needed but you can replace 6 , 9 with each other

---
+++ boj/boj1475.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class boj1475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int nums[] =new int[10];
+        int com;
+        while(t>0){
+            com= t%10;
+            t=t/10;
+            nums[com]++;
+        }
+        com = nums[6]+nums[9];
+        if(com%2==0) {
+            nums[6] = com / 2;
+            nums[9] = com / 2;
+        }else {
+            nums[6] = com / 2 +1;
+            nums[9] = com / 2 +1;
+        }
+        int max=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>max) {
+                max=nums[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
