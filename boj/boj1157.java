374a046f3c406ed49b501044b4a1fb7c210a5a85
qin0808 <qin0808@naver.com>
2021-11-26 22:01:08 +0900

    boj 1157
    get the max used alphabet
    differ between capitals as well

---
+++ boj/boj1157.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class boj1157 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        s = s.toLowerCase();
+        int[] al = new int[26];
+        for (int j = 97, i = 0; j <= 122; j++, i++) {
+            for (int k = 0; k < s.length(); k++) {
+                if(s.charAt(k) == (char)j){
+                    al[i]++;
+                }
+            }
+        }
+        int max=0;
+        int maxs=0;
+        int maxr=0;
+        for (int i =0; i<al.length;i++) {
+            if(al[i]>max){
+                max=al[i];
+                maxr = i;
+                maxs=0;
+            }else if(al[i]==max){
+                maxs++;
+            }
+        }
+        if(maxs>0){
+            System.out.println("?");
+        }else{
+            System.out.println((char)(maxr+65));
+        }
+    }
+}
