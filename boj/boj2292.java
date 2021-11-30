272859490aa45c1f204f1ed7936a8df5d3d3d148
qin040808 <qin0808@naver.com>
2021-11-30 22:26:12 +0900

    boj2292
    behive with numbers
    pick the fastest rout to the room

---
+++ boj/boj2292.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class boj2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int min=6;
+        a--;
+        int sum=1;
+        while(a>0){
+            a-=min;
+            min+=6;
+            sum++;
+        }
+        System.out.println(sum);
+    }
+}
