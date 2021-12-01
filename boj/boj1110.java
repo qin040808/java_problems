9274d5c05a47883fa1d8924dd26f8b9ca6356c13
qin040808 <qin0808@naver.com>
2021-12-01 22:47:39 +0900

    boj1110

---
+++ boj/boj1110.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class boj1110 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int x = a;
+        int w= 1;
+        while(true){
+            int b = x/10;
+            int c = x%10;
+            x=(b+c)%10+c*10;
+            if(x==a){
+                System.out.println(w);
+                break;
+            }
+            w++;
+        }
+    }
+}
