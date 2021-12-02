b1df8c2dae5bbd42fb5b4a93f1dddfd76373dc6d
qin040808 <qin0808@naver.com>
2021-12-02 23:54:42 +0900

    boj2798
    decide what's the best card to pull out from a custom black jact turn

---
+++ boj/boj2798.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class boj2798 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int be = 0;
+        int n = in.nextInt();
+        int max = in.nextInt();
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i]=in.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if(num[i]+num[j]+num[k]>be&&num[i]+num[j]+num[k]<=max){
+                        be=num[i]+num[j]+num[k];
+                    }
+                }
+            }
+        }
+        System.out.println(be);
+    }
+}
