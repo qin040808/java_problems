e52c90a7998677a45a499d99e6408ef36001dd3d
qin040808 <qin0808@naver.com>
2021-12-05 00:15:01 +0900

    boj2217
    when the answer is
    number of inputs x the min num in the inputs
    get the max result

---
+++ boj/boj2217.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj2217 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num[] =  new int[n];
+        for (int i = 0; i < num.length; i++) {
+            num[i]=sc.nextInt();
+        }
+        Arrays.sort(num);
+        int max=0;
+        for (int i = 0; i < num.length; i++) {
+            if(max<(num.length-i)*num[i]){
+                max=(num.length-i)*num[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
