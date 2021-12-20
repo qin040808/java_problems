a1a9278cd482d5065db56c2f2d0c1c5b234608ca
qin040808 <qin0808@naver.com>
2021-12-20 23:59:28 +0900

    still solving

--- boj/boj1436.java
+++ boj/boj1436.java
@@ -11,24 +11,24 @@ public class boj1436 {
         int t = 1;
         int temp;
         int num=de;
-        for (int i = 1, s = 0, k = 0; i < n; i++,k++,s++) {
-            if(s>5){
-                s=0;
-                k=0;
-                t*=10;
-            }
-            if(k>9){
-                s=0;
-                k=0;
-                c*=10;
-            }
-            if(num<se) {
-                num = s*c+de*t;
-            }else {
-                num = de*t+k*t/10;
-            }
-            System.out.println(num);
-        }
+        // for (int i = 1, s = 0, k = 0; i < n; i++,k++,s++) {
+        //     if(s>5){
+        //         s=0;
+        //         k=0;
+        //         t*=10;
+        //     }
+        //     if(k>9){
+        //         s=0;
+        //         k=0;
+        //         c*=10;
+        //     }
+        //     if(num<se) {
+        //         num = s*c+de*t;
+        //     }else {
+        //         num = de*t+k*t/10;
+        //     }
+        //     System.out.println(num);
+        // }
         for (int i = 1, s = 0, k = 0; i < n; i++,k++,s++) {
             
         }
