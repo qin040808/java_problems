25b4518682f70a1432bf8b112c1f6d3af3500dd4
qin040808 <qin0808@naver.com>
2022-01-02 20:03:03 +0900

    Update boj2869.java
    buffered reader to minmize the time

--- boj/boj2869.java
+++ boj/boj2869.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj2869 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        long a = in.nextInt();
-        long b = in.nextInt();
-        long des = in.nextInt();
-        long sum =0;
-        sum+=des%(a-b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long des = Integer.parseInt(st.nextToken());
+        long cm = (des-a)/(a-b);
+        long sum = (des-a)%(a-b) == 0 ?
+            cm + 1 : cm +2;
         System.out.println(sum);
-        in.close();
     }
 }
