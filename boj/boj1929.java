c101a4b1efcda54a1cb2c3fc7eca37afe34e3f4d
qin040808 <qin0808@naver.com>
2022-01-17 23:39:47 +0900

    boj1929
    get prime numbers

---
+++ boj/boj1929.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class boj1929 {
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int m = in.nextInt();
+		int n = in.nextInt();
+		for (int i=m; i<=n; i++) {
+			int j;
+			for (j=2; j*j<=i; j++)
+				if (i%j == 0) {
+					break;
+                }
+			if ((j*j > i)==(i != 1)) {
+                System.out.println(i);
+            }
+		}
+        in.close();
+	}
+}
