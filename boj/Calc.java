61cdfbd1c4df92781ee413acd43243863f41469e
qin040808 <qin0808@naver.com>
2022-01-26 23:39:40 +0900

    one bracket stack solved
    need to cacluate ( 2 + ( ) ) form

--- boj/Calc.java
+++ boj/Calc.java
@@ -5,13 +5,18 @@ public class Calc {
         Scanner in = new Scanner(System.in);
         String operation = in.nextLine().replaceAll("\\s","");
         Stack<String> inope =  new Stack<String>();
-        int count = 0;
+        String tempop = "";
+        int count = 0, minus = 0;
         for (int i = 0; i < operation.length(); i++) {
-            if(operation.charAt(i)=='('){
+            if(count>minus){
+                tempop=tempop+Character.toString(operation.charAt(i));
+            }else if(operation.charAt(i)=='('){
                 count++;
             }else if(operation.charAt(i)==')'){
-                count--;
-                inope.push(Character.toString(operation.charAt(i-1)));
+                minus++;
+                inope.push(calculater(tempop));
+            }else{
+                inope.push(Character.toString(operation.charAt(i)));
             }
         }
         //only use when there is * / close to () only those matter
@@ -19,7 +24,7 @@ public class Calc {
         // System.out.println(calculater(operation));
         in.close();
     }
-    public static int calculater(String operation) {
+    public static String calculater(String operation) {
         Stack<String> operator = new Stack<String>();
         String  firstope[] = operation.split("[+-]");
         for (int i = 0; i < firstope.length; i++) {
@@ -29,7 +34,7 @@ public class Calc {
                 }
             }
             String num[] = firstope[i].split("[*/]");
-            firstope[i]=Integer.toString(cacluate(num,operator));
+            firstope[i]=cacluate(num,operator);
         }
         operator.clear();
         for (String str : (operation.replaceAll("[0-9*/]","")).split("")) {
@@ -37,7 +42,7 @@ public class Calc {
         }
         return(cacluate(firstope, operator));
     }
-    public static int cacluate(String n[],Stack<String > op) {
+    public static String cacluate(String n[],Stack<String > op) {
         int sum=Integer.parseInt(n[0]);
         for (int i = 1; i < n.length && !op.isEmpty(); i++) {
             if(op.elementAt(0).equals("*")){
@@ -51,7 +56,7 @@ public class Calc {
             }
             op.remove(0);
         }
-        return sum;
+        return Integer.toString(sum);
     }
 }
     
