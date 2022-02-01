65bbeb480ad0a8f928f6f827f8b506985a279130
qin040808 <qin0808@naver.com>
2022-02-01 22:41:07 +0900

    stack with four commands

--- boj/boj10828.java
+++ boj/boj10828.java
@@ -10,17 +10,25 @@ public class boj10828 {
             line[i] = in.nextLine().split(" ");
         }
         int stack[] = new int[l];
-        for (int i = 0; i < stack.length; i++) {
+        int place = 0;
+        for (int i = 0; i < line.length; i++) {
             switch(line[i][0]) {
                 case "push":
+                    stack[place] = Integer.parseInt(line[i][1]);
+                    place++;
                     break;
                 case "pop":
+                    System.out.println(place == 0 ? -1 : stack[place-1]);
+                    place= place > 0 ? --place : 0 ;
                     break;
                 case "size":
+                    System.out.println(place);
                     break;
                 case "empty":
+                    System.out.println(place == 0 ? 1 : 0);
                     break;
                 case "top":
+                    System.out.println(place == 0 ? -1 : stack[place-1]);
                     break;
             }
         }
