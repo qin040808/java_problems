f9f952f858d9e566f62685a3e72ca1894b329bcc
qin040808 <qin0808@naver.com>
2022-02-11 23:42:54 +0900

    needs to reduce for and whie looop

--- boj/boj2164.java
+++ boj/boj2164.java
@@ -19,6 +19,7 @@ public class boj2164 {
             cards.remove(0);
             cards.push(cards.remove(0));
         }
+        // 1 1  2 2  3 2  4 4  5 2  6 4  7 6  8 8  9 2  10 4
         bw.write(Integer.toString(cards.peek())+"\n");
         bw.flush();
         bw.close();
