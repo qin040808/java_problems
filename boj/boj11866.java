e228eefac92e5cd68874e8714af4369c70fca784
qin040808 <qin0808@naver.com>
2022-02-26 23:59:25 +0900

    boj11866
    elimination numberlist

---
+++ boj/boj11866.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj11866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine().split(" ")[0]);
+    }
+}
