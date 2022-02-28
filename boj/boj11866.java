81013e49281a8690b6fdd1e23add671db27c0add
qin040808 <qin0808@naver.com>
2022-03-01 05:33:50 +0900

    boj11866

--- boj/boj11866.java
+++ boj/boj11866.java
@@ -1,14 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class boj11866 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine().split(" ")[0]);
+        String a[] = (br.readLine().split(" "));
+        int n = Integer.parseInt(a[0]);
+        int k = Integer.parseInt(a[1]) - 1;
+        Stack<Integer> st = new Stack<Integer>(); 
+        for (int i = 1; i <= n; i++) {
+            st.push(i);
+        }
+        int i = 0;
+        sb.append("<");
+        while(!st.empty()) {
+            i = (i + k >= st.size() ? (i + k) % st.size() : i + k);
+            sb.append(st.remove(i));
+            if(st.empty()){
+                sb.append(">");
+                break;
+            }
+            sb.append(", ");
+        }
+        System.out.println(sb);
     }
 }
