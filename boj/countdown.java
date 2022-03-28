dac5d95594074784ee45954ae319c251aa084e43
qin040808 <qin0808@naver.com>
2022-03-28 23:26:06 +0900

    Update countdown.java

--- boj/countdown.java
+++ boj/countdown.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class countdown {
     public static void main(String[] args) {
         double start = 35;
-        double arr[] = new double[15];
+        Scanner in = new Scanner(System.in);
+        double st = in.nextDouble();
+        int ln = in.nextInt();
+        double arr[] = new double[ln];
+        start = st;
         arr[0] = 13;
         for (int i = 1; i < 14; i++, start--) {
             arr[i] = (arr[i-1] + (100/start)); 
@@ -9,6 +15,6 @@ public class countdown {
         for (double i : arr) {
             System.out.println(i);
         }
-
+        in.close();
     }
 }
