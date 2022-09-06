a81c5d64af3ed91d940082cd85a9aa026de9e846
qin040808 <qin0808gold@gmail.com>
2022-09-06 16:11:52 +0900

    Update boj1463.java

--- boj/boj1463.java
+++ boj/boj1463.java
@@ -5,21 +5,17 @@ public class boj1463 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        int count = 0;
         int n = num;
         int a = num;
         int b = num;
-        int c;
-        while(n!=1){
-            a = n%3==0?n/3+1:a;
-            b = n%2==0?n/2+1:b;
-            c = n-1;
-            n = a<b?a<c?a:c:c<b?c:b;
-            count++;
-            if(n==1) {
-                break;
-            }
+        int arr[] = new int[num+1];
+        arr[1] = 0;
+        for (int i = 2; i <= num; i++) {
+            a = i%3==0?arr[i/3]+1:num+1;
+            b = i%2==0?arr[i/2]+1:num+1;
+            n = a<b?a:b;
+            arr[i] = arr[i-1]+1<n?arr[i-1]+1:n;
         }
-        System.out.println(count);
+        System.out.println(arr[num]);
     }
 }
